26aa20f70a566e135471f674c66efccdca5e57bf
rohit-jangra-dev <rohitjangradev@gmail.com>
2024-09-26 16:56:17 +0530

    we still rolling

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/REPO_NAME/',
+  base: '/productlistwithcart/',
   plugins: [vue()],
   resolve: {
     alias: {
